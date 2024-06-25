8415d40f4f73faa475c0771a0972b02b25c4b2b4
Madhur Gyani <madhurgyani9977@gmail.com>
2024-06-25 16:38:49 +0530

    add like icon

--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -17,6 +17,10 @@ function Registration() {
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
                 <div>
+                  <label htmlFor="fullName" className="block mb-2 text-sm font-medium text-white">Your Name</label>
+                  <input type="text" name="fullName" id="fullName" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Joh Doe" required />
+                </div>
+                <div>
                   <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
                   <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
                 </div>
